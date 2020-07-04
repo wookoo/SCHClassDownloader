ec993c4015490ca8a3b77e8fdb770b87f07c2d44
wookoo <nanayagoon@daum.net>
2020-07-04 21:10:21 +0900

    아이템 클릭시 메인 엑티비티에서 작동하게끔 수정

--- app/src/main/java/am/ze/wookoo/SCHClassDownloader/RecyclerAdapter.java
+++ app/src/main/java/am/ze/wookoo/SCHClassDownloader/RecyclerAdapter.java
@@ -12,14 +12,58 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    public  OnItemClickListener mListener = null;
+
+    public interface OnItemClickListener {
+        void onItemClick(View v, int position) ;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener)
+    {
+        this.mListener = listener;
+    }
+
+    /*
+    public static ArrayList<ClassNameData> classNameArray;
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView className;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            className = itemView.findViewById(R.id.className);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    String posClassName = classNameArray.get(pos).getClassURL();
+                    Log.d("아이템 눌렸다","아무튼 눌림 " + pos);
+                    Log.d("아이템 누른거",posClassName);
+                }
+            });
+        }
+
+    }*/
+
+    public  class MyViewHolder extends RecyclerView.ViewHolder{
         TextView className;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             className = itemView.findViewById(R.id.className);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    mListener.onItemClick(view,pos);
+                }
+            });
+
+
         }
+
     }
     private ArrayList<ClassNameData> classNameArray;
 
